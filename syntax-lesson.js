318c72ea882ae1c3a50f8fb3855d0fea30df3cf5
qaasima <qaasima@uni.coventry.ac.ul>
2022-08-24 20:39:52 +0100

    covering syntax js

---
+++ syntax-lesson.js
@@ -0,0 +1,48 @@
+//Todays lesson we will cover syntax structure for Javascript
+
+
+/* In order for us to understand javascript language
+it is essential for us to understand how to write comments */
+
+
+//So in the syntax lesson 1 we have covered a range of synstax from comments to printing variables
+
+//lets make a string variable 
+
+var string = "This is a string";
+
+//lets make a number variable
+
+var integer = 1;
+
+//lets create a variable and re-initialise that variable
+
+let dreamCar = "Mercedes";
+
+console.log(dreamCar);
+
+dreamCar = "Audi";
+
+console.log(dreamCar);
+
+//make a constant variable
+
+const Name = "abdulkadir"
+
+console.log(Name);
+
+//Print off a boolean
+
+console.log(true);
+
+//Create an undefine variable and print it off
+
+var nothing;
+
+console.log(nothing);
+
+//Create a null variable and print it off
+
+var empty = null;
+
+console.log(empty);
